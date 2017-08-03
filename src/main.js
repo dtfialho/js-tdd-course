cd8a297b1c968dfbea2874ce5ab598ecb86dc0b0
Diego T. Fialho <diego.tfialho@gmail.com>
2017-08-03 20:18:15 -0300

    Instalando e usando o eslint

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// a = 3;
+// const b = 2
+// const b = 5
+
+// const c = "asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd"
+
+const a = 3;
+const b = 2;
+const c = 'asda';
+
+function sum() {
+  return a + b + c;
+}
+
+sum();
