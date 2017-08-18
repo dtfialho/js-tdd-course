6f68f1d90e1fcf2b22eb8624172e2a5196594212
Diego T. Fialho <diego.tfialho@gmail.com>
2017-08-18 16:49:14 -0300

    Using context and refactoring test code

--- module-6/tests/main.spec.js
+++ module-6/tests/main.spec.js
@@ -2,12 +2,14 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import sinonStubPromise from 'sinon-stub-promise';
+
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/main';
+
 chai.use(sinonChai);
 sinonStubPromise(sinon);
 
 global.fetch = require('node-fetch');
 
-import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/main';
 
 describe('Spotify wrapper', () => {
 
@@ -36,23 +38,37 @@ describe('Spotify wrapper', () => {
   });
 
   describe('Generic search', () => {
+    let fetchedStub;
 
-    it('should call fetch function', () => {
-      const fetchedStub = sinon.stub(global, 'fetch');
-      const artists = search();
+    beforeEach(() => {
+      fetchedStub = sinon.stub(global, 'fetch');
+    });
 
-      expect(fetchedStub).to.have.been.calledOnce;
+    afterEach(() => {
       fetchedStub.restore();
     });
 
+    it('should call fetch function', () => {
+      const artists = search();
+
+      expect(fetchedStub).to.have.been.calledOnce;
+    });
+
     it('should receive the correct url to fetch', () => {
-      const fetchedStub = sinon.stub(global, 'fetch');
-      const artists = search('AC/DC', 'artist');
+      context('passing one type', () => {
+        const artists = search('AC/DC', 'artist');
 
-      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=AC/DC&type=artist');
+        expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=AC/DC&type=artist');
 
-      const albuns = search('AC/DC', 'album');
-      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=AC/DC&type=album');
+        const albuns = search('AC/DC', 'album');
+        expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=AC/DC&type=album');
+      });
+
+      context('passing more than one type', () => {
+        const artistAndAlbums = search('AC/DC', [ 'artist', 'album' ]);
+
+        expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=AC/DC&type=artist,album');
+      });
     });
 
   });
