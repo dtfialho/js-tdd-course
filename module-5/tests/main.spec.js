27f8b2b4ff668c00bf7de42688dac8a70a2bcdbb
Diego T. Fialho <diego.tfialho@gmail.com>
2017-08-18 10:21:39 -0300

    Chai methods

--- module-5/tests/main.spec.js
+++ module-5/tests/main.spec.js
@@ -1,33 +1,32 @@
+var expect = require('chai').expect;
+
 describe('Main', function() {
   var arr;
-
-  before(function() {
-
-  });
 
   beforeEach(function() {
     arr = [ 1, 2, 3 ];
   });
 
-  after(function() {
-
-  });
-
-  afterEach(function() {
-
+  it('should be an array', function() {
+    expect(arr).to.be.a('array');
   });
 
   it('should have a size of 4 when push another value to the array', function() {
     arr.push(4);
-    console.log(arr.length);
+    expect(arr).to.have.lengthOf(4);
   });
 
   it('should have a size of 2 when pop a value from the array', function() {
     arr.pop();
-    console.log(arr.length);
+    expect(arr).to.have.lengthOf(2);
   });
 
   it('should remove the value 3 when use pop in our array', function() {
-    console.log(arr.pop() === 3);
+    arr.pop();
+    expect(arr).to.not.include(3);
+  });
+
+  it('should return true after array.pop()', function() {
+    expect(arr.pop() === 3).to.be.true;
   });
 });
