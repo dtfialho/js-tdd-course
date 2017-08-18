9a1210ca8aea619e3eaeb17ca219c1c3d8f7965b
Diego T. Fialho <diego.tfialho@gmail.com>
2017-08-18 16:33:54 -0300

    Using calledOnce

--- module-6/tests/main.spec.js
+++ module-6/tests/main.spec.js
@@ -1,4 +1,11 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
+chai.use(sinonChai);
+sinonStubPromise(sinon);
+
+global.fetch = require('node-fetch');
 
 import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/main';
 
@@ -31,7 +38,10 @@ describe('Spotify wrapper', () => {
   describe('Generic search', () => {
 
     it('should call fetch function', () => {
+      const fetchedStub = sinon.stub(global, 'fetch');
       const artists = search();
+
+      expect(fetchedStub).to.have.been.calledOnce;
     });
 
   });
