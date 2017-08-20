b54aa2174478dc4af8a837b41aab07fe0d8102bc
Diego T. Fialho <diego.tfialho@gmail.com>
2017-08-20 15:53:41 -0300

    Returning promises (finished)

--- module-6/src/main.js
+++ module-6/src/main.js
@@ -1,8 +1,7 @@
-export const search = (query, type) => {
+export const search = (query, type) =>
   fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`)
-    .then(data => data.json())
-    .catch(err => err);
-};
+    .then(data => data.json());
+
 export const searchAlbums = () => {};
 export const searchArtists = () => {};
 export const searchTracks = () => {};
