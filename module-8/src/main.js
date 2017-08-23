02931be102f6caf4ca5e71500efbe5bf35723d06
Diego T. Fialho <diego.tfialho@gmail.com>
2017-08-23 11:01:42 -0300

    Adding album-info to the layout

--- module-8/src/main.js
+++ module-8/src/main.js
@@ -1,7 +1,13 @@
 import spotify from './Spotify';
 import renderAlbums from './AlbumList';
+import renderAlbumInfo from './AlbumInfo';
 
 const albums = spotify.search.albums('AC/DC');
 const albumList = document.getElementById('album-list');
 
+const album = spotify.album.getAlbum('6mUdeDZCsExyJLMdAfDuwh');
+const albumInfo = document.getElementById('album-info');
+
 albums.then(data => renderAlbums(data.albums.items, albumList));
+
+album.then(data => renderAlbumInfo(data, albumInfo));
