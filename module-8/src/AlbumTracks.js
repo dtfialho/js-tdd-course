ac25aa49a1ca2e719627d80d9a95f81d732b37b4
Diego T. Fialho <diego.tfialho@gmail.com>
2017-08-23 11:59:55 -0300

    Adding convertTime to the layout

--- module-8/src/AlbumTracks.js
+++ module-8/src/AlbumTracks.js
@@ -1,9 +1,11 @@
+import convertTime from './ConvertTime';
+
 function createMarkup(data) {
   return data.map(track => `
     <div class="music" data-track-preview="${track.preview_url}">
       <p class="music-number">${track.track_number}</p>
       <p class="music-title">${track.name}</p>
-      <p class="music-duration">${track.duration_ms}</p>
+      <p class="music-duration">${convertTime(track.duration_ms)}</p>
     </div>`).join('');
 }
 
--- module-8/tests/AlbumTracks.spec.js
+++ module-8/tests/AlbumTracks.spec.js
@@ -2,6 +2,7 @@ import 'jsdom-global/register';
 import { expect } from 'chai';
 
 import renderAlbumTracks from '../src/AlbumTracks';
+import convertTime from '../src/ConvertTime';
 
 describe('AlbumTracks', () => {
 
@@ -33,19 +34,19 @@ describe('AlbumTracks', () => {
     <div class="music" data-track-preview="https://p.scdn.co/mp3-preview/63c396837c1738dd17a7a2b055a4981dce74f46f?cid=8897482848704f2a8f8d7c79726a70d4">
       <p class="music-number">1</p>
       <p class="music-title">Hells Bells</p>
-      <p class="music-duration">312293</p>
+      <p class="music-duration">${convertTime(312293)}</p>
     </div>`;
 
   const markup2 = `
     <div class="music" data-track-preview="https://p.scdn.co/mp3-preview/63c396837c1738dd17a7a2b055a4981dce74f46f?cid=8897482848704f2a8f8d7c79726a70d4">
       <p class="music-number">1</p>
       <p class="music-title">Hells Bells</p>
-      <p class="music-duration">312293</p>
+      <p class="music-duration">${convertTime(312293)}</p>
     </div>
     <div class="music" data-track-preview="https://p.scdn.co/mp3-preview/63c396837c1738dd17a7a2b055a4981dce74f46f?cid=8897482848704f2a8f8d7c79726a70d4">
       <p class="music-number">1</p>
       <p class="music-title">Hells Bells</p>
-      <p class="music-duration">312293</p>
+      <p class="music-duration">${convertTime(312293)}</p>
     </div>`;
 
   it('should create and append the markup given a correct data', () => {
