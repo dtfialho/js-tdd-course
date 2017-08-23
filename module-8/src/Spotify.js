241ab7d91f6ef53991f30ad5764bb19807f18048
Diego T. Fialho <diego.tfialho@gmail.com>
2017-08-23 10:22:48 -0300

    Adding album-list do the layout

--- module-8/src/Spotify.js
+++ module-8/src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
-  token: ''
+  token: 'BQBUXfNie_dzqDPI_83BMOCQ_uw4dsSdp75vqdMPEbSY2di8dZNRuTc9e0kBh5IY7PKiTvTXV2Ta1tV1uQOL5ODHbpTxP_W74I9Tu5mx8-gn6sMdgLKYHsR5a_zE6ZgTeKL3wbbst0WG_ZO-'
 });
 
 export default spotify;
--- module-8/src/main.js
+++ module-8/src/main.js
@@ -1,1 +1,7 @@
-console.log('Spotify Player');
+import spotify from './Spotify';
+import renderAlbums from './AlbumList';
+
+const albums = spotify.search.albums('AC/DC');
+const albumList = document.getElementById('album-list');
+
+albums.then(data => renderAlbums(data.albums.items, albumList));
