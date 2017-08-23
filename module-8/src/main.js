801c6bf47f4697ba67f49323a2e32570a7c5e403
Diego T. Fialho <diego.tfialho@gmail.com>
2017-08-23 14:40:47 -0300

    Creating the PlaylistTrigger

--- module-8/src/main.js
+++ module-8/src/main.js
@@ -1,6 +1,8 @@
 import spotify from './Spotify';
 import searchEnterTrigger from './SearchTrigger';
 import selectAlbumTrigger from './SelectAlbumTrigger';
+import playlistTrigger from './PlaylistTrigger';
 
 searchEnterTrigger();
 selectAlbumTrigger();
+playlistTrigger();
