aeba75aa1f623b8d08c952b97e3875fe09b78688
Diego T. Fialho <diego.tfialho@gmail.com>
2017-09-02 13:04:10 -0300

    Adding a loader

--- module-9/src/ConvertBTC.js
+++ module-9/src/ConvertBTC.js
@@ -1,10 +1,18 @@
 const chalk = require('chalk');
 const request = require('request');
+const ora = request('ora');
+
+const spinner = ora({
+  text: 'Retrieving Bitcoin data...',
+  color: 'yellow'
+});
 
 function convertBTC(currency = 'USD', amount = 1) {
   const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${currency}&amount=${amount}`;
 
+  spinner.start();
   request(url, (error, response, body) => {
+    spinner.stop();
     let apiResponse;
     try {
       apiResponse = JSON.parse(body)
