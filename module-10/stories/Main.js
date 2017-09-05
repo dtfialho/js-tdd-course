bcd9e01e65630a0008b602c08cc9011c9bff24e2
Diego T. Fialho <diego.tfialho@gmail.com>
2017-09-05 14:09:34 -0300

    Writing stories

--- module-10/stories/Main.js
+++ module-10/stories/Main.js
@@ -1,5 +1,54 @@
 import React from 'react';
-import Component from '../src/Main'; // This is our component
+import FullHeader from '../src/Main'; // This is our component
 import { storiesOf } from '@storybook/react';
 
-storiesOf('Component', module)
+storiesOf('React Full Header', module)
+    .add('with title', () => {
+        <FullHeader title='TDD' />
+    })
+    .add('with title and subtitle', () => {
+        <FullHeader
+            title="TDD"
+            subtitle="What is tested may never break."
+        />
+    })
+    .add('with title, subtitle, bgColor', () => {
+        <FullHeader
+            title="TDD"
+            subtitle="What is tested may never break."
+            bgColor="#3299BB"
+        />
+    })
+    .add('with title, subtitle, bgColor, textColor', () => {
+        <FullHeader
+            title="TDD"
+            subtitle="What is tested may never break."
+            bgColor="#3299BB"
+            textColor="#FF9900"
+        />
+    })
+    .add('with title, subtitle, bgColor, textColor, font', () => {
+        <FullHeader
+            title="TDD"
+            subtitle="What is tested may never break."
+            bgColor="#3299BB"
+            textColor="#FF9900"
+            font="cursive"
+        />
+    })
+    .add('with title, subtitle, bgImg', () => {
+        <FullHeader
+            title="TDD"
+            subtitle="What is tested may never break."
+            bgImg="https://raw.githubusercontent.com/willianjusten/photo-examples/master/iceland-glacier.jpg"
+        />
+    })
+    .add('with title, subtitle, video', () => {
+        <FullHeader
+            title="TDD"
+            subtitle="What is tested may never break."
+            bgColor="#EBE9EB"
+            textColor="#3299BB"
+            video="http://callmenick.com/_development/video/full-screen-background-video/media/demo.mp4"
+        />
+    })
